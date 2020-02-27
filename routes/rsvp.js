8378411117b7df775bb3f0acff810c1cde2cfc84
Jiahao Lou <jil838@ucsd.edu>
2020-02-26 18:36:03 -0800

    lab8

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,13 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+  // This will print to your terminal whenever the POST request is made
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail); // Add to current data
+
+  // Let us send back some data so that browser knows it worked.
+  response.send(rsvpEmail);
+};
